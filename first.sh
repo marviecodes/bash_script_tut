b655b27e930836f4dbe8d2fc82003e1b1c339025
Marvelous Osondu <marvnonso554@gmail.com>
2024-12-17 13:56:05 +0100

    Update files

--- first.sh
+++ first.sh
@@ -19,3 +19,9 @@ echo $food
 # Writing to a file
 echo "some file content" > output.txt
 echo "some more file content" >> output.txt
+
+main
+    feature1
+    feature2
+    feature3
+Added something on feature3
---
+++ functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is a function
+greet_user() {
+    echo "Hello, Techies! Today is: " `date`
+}
+
+# This is another function
+cook() {
+    echo "Hello, i am cooking"
+}
